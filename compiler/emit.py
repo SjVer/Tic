b9bd4de2f91407bbe9767524e3a38038191c2f4f
SjVer <sjoerd@marsenaar.com>
2021-06-07 10:36:10 +0200

    Added option to preserve c file for compiler

--- compiler/emit.py
+++ compiler/emit.py
@@ -1,63 +1,75 @@
-import os
+import os, subprocess
 
 # Emitter object keeps track of the generated code and outputs it.
 class Emitter:
-    def __init__(self, tempfile, verbose):
-        self.tempfile = tempfile
-        self.verbose = verbose
+	def __init__(self, tempfile, verbose, keep_c_file):
+		self.tempfile = tempfile
+		self.verbose = verbose
+		self.keep_c_file = keep_c_file
 
-        self.override_emit_to_func = False
+		self.override_emit_to_func = False
 
-        self.includes = ""
-        self.wrappers = ""
-        self.functions = ""
-        self.header = ""
-        self.code = ""
+		self.includes = ""
+		self.wrappers = ""
+		self.functions = ""
+		self.header = ""
+		self.code = ""
 
-    def emit(self, code):
-        if self.verbose:
-            print("EMIT: " + code)
-        if self.override_emit_to_func:
-            self.functions += code + '\n'
-        else:
-            self.code += code + '\n'
+	def emit(self, code):
+		if self.verbose:
+			print("EMIT: " + code)
+		if self.override_emit_to_func:
+			self.functions += code + '\n'
+		else:
+			self.code += code + '\n'
 
-    def emitLine(self, code):
-        if self.verbose:
-            print("EMIT: " + code)
-        if self.override_emit_to_func:
-            self.functions += code + '\n'
-        else:
-            self.code += code + '\n'
+	def emitLine(self, code):
+		if self.verbose:
+			print("EMIT: " + code)
+		if self.override_emit_to_func:
+			self.functions += code + '\n'
+		else:
+			self.code += code + '\n'
 
-    def headerLine(self, code):
-        if self.verbose:
-            print("EMIT HEADER: " + code)
-        self.header += code + '\n'
+	def headerLine(self, code):
+		if self.verbose:
+			print("EMIT HEADER: " + code)
+		self.header += code + '\n'
 
-    def includeLine(self, code):
-        if self.verbose:
-            print("EMIT INCLUDE: " + code)
-        self.includes += code + '\n'
+	def includeLine(self, code):
+		if self.verbose:
+			print("EMIT INCLUDE: " + code)
+		self.includes += code + '\n'
 
-    def wrapperFunc(self, code):
-        if self.verbose:
-            print("EMIT WRAPPER: " + code)
-        self.wrappers += code + '\n'
+	def wrapperFunc(self, code):
+		if self.verbose:
+			print("EMIT WRAPPER: " + code)
+		self.wrappers += code + '\n'
 
-    def function(self, code):
-        if self.verbose:
-            print("EMIT FUNCTION: " + code)
-        self.functions += code
+	def function(self, code):
+		if self.verbose:
+			print("EMIT FUNCTION: " + code)
+		self.functions += code
 
-    def writeFile(self):
-        text = self.includes + "\n\n\n" + self.header + "\n\n//funcs:\n\n" + self.functions + "\n\n//wraps:\n\n" + self.wrappers + "\n\n//code:\n\nint main(void){\n" + self.code + "\nreturn 0;\n}"
-        self.tempfile.write(text)
-        self.tempfile.seek(0)
-        # print("\nCode:\n\n" + text + "\n\n\n")
-        if self.verbose:
-            print('\n\ngenerated code:\n--------------------------\n')
-            os.system('clang-format ' + self.tempfile.name)        
-            print('\n\n--------------------------\n\n')
-        #with open(self.tempfile, 'w') as outputFile:
-        #    outputFile.write(self.header + self.code)
+	def writeFile(self):
+		code = self.includes + "\n\n\n" + self.header + "\n\n//funcs:\n\n" + self.functions + "\n\n//wraps:\n\n" + self.wrappers + "\n\n//code:\n\nint main(void){\n" + self.code + "\nreturn 0;\n}"
+
+		if not self.keep_c_file:
+			self.tempfile.write(code)
+			self.tempfile.seek(0)
+
+		else:
+			with open(self.tempfile, 'w') as f:
+				f.write(code)
+
+			text = subprocess.Popen(["clang-format", self.tempfile], stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
+
+			with open(self.tempfile, 'w') as f:
+				f.write(text)
+
+
+		if self.verbose:
+			print('\n\ngenerated code:\n--------------------------\n')
+			print(text)
+			#os.system('clang-format ' + self.tempfile.name)        
+			print('\n\n--------------------------\n\n')
